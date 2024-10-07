9abd08de773907755c6ddcbb7f466cffe27b84b9
PJ Reiniger <pj.reiniger@gmail.com>
2024-10-07 03:58:31 -0400

    Auto Update to '2025.0.0-beta-1.1' (#39)
    Co-authored-by: pjreiniger <10381694+pjreiniger@users.noreply.github.com>

--- tests/java/pathplannerlibjava/BasicTest.java
+++ tests/java/pathplannerlibjava/BasicTest.java
@@ -5,19 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.path.PathPlannerTrajectory;
+import com.pathplanner.lib.path.Waypoint;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class BasicTest {
   @Test
   void basicTest() {
-    List<Translation2d> bezierPoints =
-        PathPlannerPath.bezierFromPoses(
+    List<Waypoint> bezierPoints =
+        PathPlannerPath.waypointsFromPoses(
             new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
             new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
             new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90)));
@@ -26,10 +24,9 @@ class BasicTest {
         new PathPlannerPath(
             bezierPoints,
             new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI),
+            null,
             new GoalEndState(0.0, Rotation2d.fromDegrees(-90)));
 
-    PathPlannerTrajectory traj1 =
-        new PathPlannerTrajectory(path, new ChassisSpeeds(), Rotation2d.fromDegrees(0));
-    assertTrue(traj1.getStates().size() > 0);
+    assertTrue(path.getWaypoints().size() > 0);
   }
 }
