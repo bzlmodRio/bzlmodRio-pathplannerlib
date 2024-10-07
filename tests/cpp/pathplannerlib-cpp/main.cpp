c30e240ed0db73f3c28af457747a418085918104
PJ Reiniger <pj.reiniger@gmail.com>
2024-10-07 03:39:28 -0400

    Fix compilation

--- tests/cpp/pathplannerlib-cpp/main.cpp
+++ tests/cpp/pathplannerlib-cpp/main.cpp
@@ -1,6 +1,6 @@
 
 #include <pathplanner/lib/path/PathPlannerPath.h>
-#include <pathplanner/lib/path/PathPlannerTrajectory.h>
+#include <pathplanner/lib/trajectory/PathPlannerTrajectory.h>
 
 #include <iostream>
 
@@ -11,11 +11,12 @@ int main() {
       frc::Pose2d(1.0_m, 1.0_m, frc::Rotation2d(0_deg)),
       frc::Pose2d(3.0_m, 1.0_m, frc::Rotation2d(0_deg)),
       frc::Pose2d(5.0_m, 3.0_m, frc::Rotation2d(90_deg))};
-  std::vector<frc::Translation2d> bezierPoints =
-      PathPlannerPath::bezierFromPoses(poses);
+  std::vector<pathplanner::Waypoint> bezierPoints =
+      PathPlannerPath::waypointsFromPoses(poses);
 
   auto path = std::make_shared<PathPlannerPath>(
       bezierPoints,
       PathConstraints(3.0_mps, 3.0_mps_sq, 360_deg_per_s, 720_deg_per_s_sq),
+      std::nullopt,
       GoalEndState(0.0_mps, frc::Rotation2d(-90_deg)));
 }
--- tests/java/pathplannerlibjava/Main.java
+++ tests/java/pathplannerlibjava/Main.java
@@ -3,19 +3,17 @@ package pathplannerlibjava;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.path.PathPlannerTrajectory;
+import com.pathplanner.lib.path.Waypoint;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import java.util.List;
 
 public final class Main {
   private Main() {}
 
   public static void main(String[] args) {
-    List<Translation2d> bezierPoints =
-        PathPlannerPath.bezierFromPoses(
+    List<Waypoint> bezierPoints =
+        PathPlannerPath.waypointsFromPoses(
             new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
             new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
             new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90)));
@@ -24,10 +22,9 @@ public final class Main {
         new PathPlannerPath(
             bezierPoints,
             new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI),
+            null,
             new GoalEndState(0.0, Rotation2d.fromDegrees(-90)));
 
-    PathPlannerTrajectory traj1 =
-        new PathPlannerTrajectory(path, new ChassisSpeeds(), Rotation2d.fromDegrees(0));
-    System.out.println(traj1);
+    System.out.println(path);
   }
 }
